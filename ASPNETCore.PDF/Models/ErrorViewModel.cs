75e493d2b5b1eaa482bf73df258fb9cdc5050315
Dara Oladapo <daraoladapo@outlook.com>
2019-03-19 20:25:16 +0100

    reog

---
+++ ASPNETCore.PDF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCore.PDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
